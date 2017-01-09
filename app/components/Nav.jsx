2dbbf3c77496d201d6ba5ec22f73aa59279cf5a7
Enrico Smith <EnricoS@eohmc.co.za>
2017-01-09 11:45:54 +0200

    Update copy

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Nav = () => {
-  return (
+  return ( 
     <div>
       <h2>Nav Component</h2>
       <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
