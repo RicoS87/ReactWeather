4de5cf9c670097be4efa1ba4fa57c6e3a431c375
Enrico Smith <EnricoS@eohmc.co.za>
2017-01-10 10:32:01 +0200

    add app.css style

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -5,6 +5,7 @@ var WeatherForm = require('WeatherForm');
 var openWeatherMap = require('openWeatherMap');
 var ErrorModal = require('ErrorModal');
 
+
 var Weather = React.createClass({
   getDefaultProps: function () {
     return {location: ''}
@@ -57,7 +58,7 @@ var Weather = React.createClass({
 
     return (
       <div>
-        <h1 className="text-center">Get Weather</h1>
+        <h1 className="text-center page-title">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
         {renderError()}
