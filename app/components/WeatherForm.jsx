fd0950f0559b6e7e879147f562d15d6dfdec3e51
Enrico Smith <EnricoS@eohmc.co.za>
2017-01-10 10:15:40 +0200

    add error modal

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -1,22 +1,40 @@
 var React = require('react');
 
 var WeatherForm = React.createClass({
+  // getDefaultProps: function() {
+  //   return {
+  //     errorMessage: 'Error'
+  //   }
+  // },
   onFormSubmit: function (e) {
     e.preventDefault();
     var location = this.refs.location.value;
+    var errorMessage = this.props.errorMessage;
     if(location.length > 0)
     {
       this.refs.location.value = '';
       this.props.onSearch(location);
+    } else {
+      // errorMessage = 'Location cannot be empty.';
     }
+
   },
   render: function() {
+    // function renderError () {
+    //   if(typeof errorMessage === 'string') {
+    //     return (
+    //       <ErrorModal message={errorMessage}/>
+    //     )
+    //   }
+    // }
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
           <input type="text" ref="location" placeholder="Enter city name" />
           <button className="button expanded hollow">Get Weather</button>
+          {/* {renderError()} */}
         </form>
+
       </div>
     );
   }
